2ea59709c8a470dddc2f96c39a3eaede1866e692
sukyungdev <sukyung.developer@gmail.com>
2022-10-23 23:41:04 +0900

    add : .prettierrc

--- src/components/List.js
+++ src/components/List.js
@@ -1,57 +1,53 @@
-import React from "react";
-import { BsCheck } from "react-icons/bs";
+import React from 'react';
+import { BsCheck } from 'react-icons/bs';
 const List = React.memo(
-    ({ id, content, completed, toDoData, setToDoData, provided, snapshot, completeChange }) => {
-        console.log("List Component");
+  ({ id, content, completed, toDoData, setToDoData, provided, snapshot, completeChange }) => {
+    console.log('List Component');
 
-        const checkChange = (id) => {
-            let newToDoData = toDoData.filter((data) => data.id !== id);
-            console.log("newToDoData", newToDoData);
-            setToDoData(newToDoData);
-        };
-        return (
-            <div
-                key={id}
-                {...provided.draggableProps}
-                ref={provided.innerRef}
-                {...provided.dragHandleProps}
-                className={`${
-                    completed ? "bg-green-200" : "bg-purple-100"
-                } p-3 mb-3 rounded shadow flex justify-between items-center`}
-            >
-                <div className="flex items-center">
-                    <input
-                        className="mr-2 hidden"
-                        id={id}
-                        type="checkbox"
-                        defaultChecked={false}
-                        onChange={() => completeChange(id)}
-                    />
-                    <label
-                        htmlFor={id}
-                        className="w-7 h-7 mr-3 rounded-full border-2 border-purple-400 bg-amber-300 flex items-center justify-center"
-                    >
-                        {completed ? <BsCheck size={50} color={"purple"} /> : ""}
-                    </label>
-                    <span
-                        className={`${
-                            completed ? "line-through" : undefined
-                        } text-gray-500 mr-2 text-lg`}
-                    >
-                        {content}
-                    </span>
-                </div>
-                <div className="flex items-center">
-                    <button
-                        className="px-2.5 pb-0.5 text-gray-600 text-lg"
-                        type="button"
-                        onClick={() => checkChange(id)}
-                    >
-                        x
-                    </button>
-                </div>
-            </div>
-        );
-    }
+    const checkChange = (id) => {
+      let newToDoData = toDoData.filter((data) => data.id !== id);
+      console.log('newToDoData', newToDoData);
+      setToDoData(newToDoData);
+    };
+    return (
+      <div
+        key={id}
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+        {...provided.dragHandleProps}
+        className={`${
+          completed ? 'bg-green-200' : 'bg-purple-100'
+        } p-3 mb-3 rounded shadow flex justify-between items-center`}
+      >
+        <div className="flex items-center">
+          <input
+            className="mr-2 hidden"
+            id={id}
+            type="checkbox"
+            defaultChecked={false}
+            onChange={() => completeChange(id)}
+          />
+          <label
+            htmlFor={id}
+            className="w-7 h-7 mr-3 rounded-full border-2 border-purple-400 bg-amber-300 flex items-center justify-center"
+          >
+            {completed ? <BsCheck size={50} color={'purple'} /> : ''}
+          </label>
+          <span className={`${completed ? 'line-through' : undefined} text-gray-500 mr-2 text-lg`}>
+            {content}
+          </span>
+        </div>
+        <div className="flex items-center">
+          <button
+            className="px-2.5 pb-0.5 text-gray-600 text-lg"
+            type="button"
+            onClick={() => checkChange(id)}
+          >
+            x
+          </button>
+        </div>
+      </div>
+    );
+  },
 );
 export default List;
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,59 +1,55 @@
-import React from "react";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
-import List from "./List";
+import List from './List';
 
 const Lists = React.memo(({ toDoData, setToDoData, completeChange }) => {
-    console.log("Lists Component");
-    //Drag and Drop
-    const changeList = (info) => {
-        //item info check
-        console.log(info);
-        if (!info.destination) return;
-        const newToDoData = toDoData;
+  console.log('Lists Component');
+  //Drag and Drop
+  const changeList = (info) => {
+    //item info check
+    console.log(info);
+    if (!info.destination) return;
+    const newToDoData = toDoData;
 
-        //item index change
-        const [listChangeItem] = newToDoData.splice(info.source.index, 1);
-        newToDoData.splice(info.destination.index, 0, listChangeItem);
+    //item index change
+    const [listChangeItem] = newToDoData.splice(info.source.index, 1);
+    newToDoData.splice(info.destination.index, 0, listChangeItem);
 
-        //state change
-        setToDoData(newToDoData);
-    };
+    //state change
+    setToDoData(newToDoData);
+  };
 
-    // console.log(toDoData);
-    return (
-        <div>
-            <DragDropContext onDragEnd={changeList}>
-                <Droppable droppableId="list">
-                    {(provided) => (
-                        <div {...provided.droppableProps} ref={provided.innerRef}>
-                            {toDoData.map((data, index) => (
-                                <Draggable
-                                    key={data.id}
-                                    draggableId={data.id.toString()}
-                                    index={index}
-                                >
-                                    {(provided, snapshot) => (
-                                        <List
-                                            completeChange={completeChange}
-                                            id={data.id}
-                                            content={data.content}
-                                            completed={data.completed}
-                                            toDoData={toDoData}
-                                            setToDoData={setToDoData}
-                                            provided={provided}
-                                            snapshot={snapshot}
-                                        />
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
-        </div>
-    );
+  // console.log(toDoData);
+  return (
+    <div>
+      <DragDropContext onDragEnd={changeList}>
+        <Droppable droppableId="list">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {toDoData.map((data, index) => (
+                <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
+                  {(provided, snapshot) => (
+                    <List
+                      completeChange={completeChange}
+                      id={data.id}
+                      content={data.content}
+                      completed={data.completed}
+                      toDoData={toDoData}
+                      setToDoData={setToDoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 });
 
 export default Lists;
